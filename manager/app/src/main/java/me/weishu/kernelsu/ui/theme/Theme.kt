e8e2a1fc3b1d32c59e2784cae26f1e36b0143cc9
rsuntk <rsuntk@yukiprjkt.my.id>
2025-08-24 23:24:07 +0700

    manager: initial expressive
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/theme/Theme.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/theme/Theme.kt
@@ -2,11 +2,11 @@ package me.weishu.kernelsu.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.expressiveLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -35,10 +35,10 @@ fun KernelSUTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> expressiveLightColorScheme()
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
