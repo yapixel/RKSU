6100ab6e14da66f882866208ebc1f746fa035705
rsuntk <rsuntk@yukiprjkt.my.id>
2025-09-07 09:39:46 +0700

    kernel: kill ksu_is_compat
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -27,8 +27,6 @@
 #include "kernel_compat.h"
 #include "selinux/selinux.h"
 
-bool ksu_is_compat __read_mostly = false; // let it here
-
 static const char KERNEL_SU_RC[] =
 	"\n"
 
@@ -109,7 +107,6 @@ static const char __user *get_user_arg_ptr(struct user_arg_ptr argv, int nr)
 		if (get_user(compat, argv.ptr.compat + nr))
 			return ERR_PTR(-EFAULT);
 
-		ksu_is_compat = true;
 		return compat_ptr(compat);
 	}
 #endif
