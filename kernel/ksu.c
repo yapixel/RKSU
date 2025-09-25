8d135d430c61f623f6c054bc8a088261970e2272
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-25 20:24:36 +0700

    kernel: print some quirks
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -2,6 +2,9 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
+#include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 #include <linux/workqueue.h>
 
 #include "allowlist.h"
@@ -10,24 +13,6 @@
 #include "klog.h" // IWYU pragma: keep
 #include "ksu.h"
 #include "throne_tracker.h"
-
-#ifdef CONFIG_KSU_CMDLINE
-#include <linux/init.h>
-
-// use get_ksu_state()!
-unsigned int enable_kernelsu = 1; // enabled by default
-static int __init read_kernelsu_state(char *s)
-{
-	if (s)
-		enable_kernelsu = simple_strtoul(s, NULL, 0);
-	return 1;
-}
-__setup("kernelsu.enabled=", read_kernelsu_state);
-
-bool get_ksu_state(void) { return enable_kernelsu >= 1; }
-#else
-bool get_ksu_state(void) { return true; }
-#endif /* CONFIG_KSU_CMDLINE */
 
 static struct workqueue_struct *ksu_workqueue;
 
@@ -57,15 +42,9 @@ extern void ksu_ksud_exit(void);
 
 int __init kernelsu_init(void)
 {
-	pr_info("kernelsu.enabled=%d\n",
-		(int)get_ksu_state());
-
-#ifdef CONFIG_KSU_CMDLINE
-	if (!get_ksu_state()) {
-		pr_info_once("drivers is disabled.");
-		return 0;
-	}
-#endif
+	/* Print quirks for easier debug */
+	pr_info("Initialized on: %s (%s) with kernelsu driver version: %u\n",
+		UTS_RELEASE, UTS_MACHINE, KSU_VERSION);
 
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
@@ -89,8 +68,6 @@ int __init kernelsu_init(void)
 
 #ifdef CONFIG_KSU_KPROBES_HOOK
 	ksu_ksud_init();
-#else
-	pr_debug("init ksu driver\n");
 #endif
 
 #ifdef MODULE
@@ -103,11 +80,6 @@ int __init kernelsu_init(void)
 
 void kernelsu_exit(void)
 {
-#ifdef CONFIG_KSU_CMDLINE
-	if (!get_ksu_state()) {
-		return;
-	}
-#endif
 	ksu_allowlist_exit();
 
 	ksu_throne_tracker_exit();
@@ -129,7 +101,6 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
 MODULE_DESCRIPTION("Android KernelSU");
 
-#include <linux/version.h>
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
 #endif
