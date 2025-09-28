saadelasfur/KernelSU-Next

manager: add support for opening zip file and directly flash module

manager/app/src/main/java/com/rifsxd/ksunext/ui/MainActivity.kt
@@ -55,6 +55,10 @@ import com.rifsxd.ksunext.ui.util.LocalSnackbarHost
 import com.rifsxd.ksunext.ui.util.rootAvailable
 import com.rifsxd.ksunext.ui.util.install
 
+import android.content.Intent
+import android.net.Uri
+import com.rifsxd.ksunext.ui.screen.FlashIt
+
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,6 +74,14 @@ class MainActivity : ComponentActivity() {
         val isManager = Natives.becomeManager(ksuApp.packageName)
         if (isManager) install()
 
+        // Check if launched with a ZIP file
+        val zipUri: Uri? = when (intent?.action) {
+            Intent.ACTION_VIEW, Intent.ACTION_SEND -> {
+                intent.data ?: intent.getParcelableExtra(Intent.EXTRA_STREAM)
+            }
+            else -> null
+        }?.takeIf { it.toString().endsWith(".zip", ignoreCase = true) }
+
         setContent {
             // Read AMOLED mode preference
             val prefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
@@ -81,6 +93,18 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val snackBarHostState = remember { SnackbarHostState() }
                 val currentDestination = navController.currentBackStackEntryAsState()?.value?.destination
+
+                val navigator = navController.rememberDestinationsNavigator()
+
+                LaunchedEffect(zipUri) {
+                    if (zipUri != null) {
+                        navigator.navigate(
+                            FlashScreenDestination(
+                                FlashIt.FlashModules(listOf(zipUri))
+                            )
+                        )
+                    }
+                }
 
                 val showBottomBar = when (currentDestination?.route) {
                     FlashScreenDestination.route -> false // Hide for FlashScreenDestination

manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Flash.kt
@@ -63,6 +63,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import com.rifsxd.ksunext.R
+import com.rifsxd.ksunext.ui.component.rememberConfirmDialog
+import com.rifsxd.ksunext.ui.component.ConfirmResult
 import com.rifsxd.ksunext.ui.component.KeyEventBlocker
 import com.rifsxd.ksunext.ui.util.FlashResult
 import com.rifsxd.ksunext.ui.util.LkmSelection
@@ -138,12 +140,41 @@ fun FlashScreen(navigator: DestinationsNavigator, flashIt: FlashIt) {
         // Disable back button if flashing is running
     }
 
-    LaunchedEffect(Unit) {
-        if (text.isNotEmpty()) {
-            return@LaunchedEffect
+    val confirmDialog = rememberConfirmDialog()
+    var confirmed by rememberSaveable { mutableStateOf(flashIt !is FlashIt.FlashModules) }
+    var pendingFlashIt by rememberSaveable { mutableStateOf<FlashIt?>(null) }
+
+    LaunchedEffect(flashIt) {
+        if (flashIt is FlashIt.FlashModules && !confirmed) {
+            val uris = flashIt.uris
+            val moduleNames =
+                uris.mapIndexed { index, uri -> "\n${index + 1}. ${uri.getFileName(context)}" }
+                    .joinToString("")
+            val confirmContent =
+                context.getString(R.string.module_install_prompt_with_name, moduleNames)
+            val confirmTitle = context.getString(R.string.module)
+            val result = confirmDialog.awaitConfirm(
+                title = confirmTitle,
+                content = confirmContent,
+                markdown = true
+            )
+            if (result == ConfirmResult.Confirmed) {
+                confirmed = true
+                pendingFlashIt = flashIt
+            } else {
+                // User cancelled, go back
+                navigator.popBackStack()
+            }
+        } else {
+            confirmed = true
+            pendingFlashIt = flashIt
         }
+    }
+
+    LaunchedEffect(confirmed, pendingFlashIt) {
+        if (!confirmed || pendingFlashIt == null || text.isNotEmpty()) return@LaunchedEffect
         withContext(Dispatchers.IO) {
-            flashIt(flashIt, onStdout = {
+            flashIt(pendingFlashIt!!, onStdout = {
                 tempText = "$it\n"
                 if (tempText.startsWith("[H[J")) { // clear command
                     text = tempText.substring(6)
@@ -280,6 +311,19 @@ fun FlashScreen(navigator: DestinationsNavigator, flashIt: FlashIt) {
             )
         }
     }
+}
+
+fun Uri.getFileName(context: Context): String {
+    val contentResolver = context.contentResolver
+    val cursor = contentResolver.query(this, null, null, null, null)
+    return cursor?.use {
+        val nameIndex = it.getColumnIndex(android.provider.OpenableColumns.DISPLAY_NAME)
+        if (it.moveToFirst() && nameIndex != -1) {
+            it.getString(nameIndex)
+        } else {
+            this.lastPathSegment ?: "unknown.zip"
+        }
+    } ?: (this.lastPathSegment ?: "unknown.zip")
 }
 
 @Parcelize

yapixel/RKSU

Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package me.weishu.kernelsu.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -32,6 +34,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -44,12 +47,14 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.animations.NavHostAnimatedDestinationStyle
 import com.ramcosta.composedestinations.generated.destinations.ExecuteModuleActionScreenDestination
+import com.ramcosta.composedestinations.generated.destinations.FlashScreenDestination
 import com.ramcosta.composedestinations.generated.NavGraphs
 import com.ramcosta.composedestinations.utils.isRouteOnBackStackAsState
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 import me.weishu.kernelsu.Natives
 import me.weishu.kernelsu.ksuApp
 import me.weishu.kernelsu.ui.screen.BottomBarDestination
+import me.weishu.kernelsu.ui.screen.FlashIt
 import me.weishu.kernelsu.ui.theme.KernelSUTheme
 import me.weishu.kernelsu.ui.util.LocalSnackbarHost
 import me.weishu.kernelsu.ui.util.rootAvailable
@@ -71,6 +76,14 @@ class MainActivity : ComponentActivity() {
         val isManager = Natives.becomeManager(ksuApp.packageName)
         if (isManager) install()
 
+        // Check if launched with a ZIP file
+        val zipUri: Uri? = when (intent?.action) {
+            Intent.ACTION_VIEW, Intent.ACTION_SEND -> {
+                intent.data ?: intent.getParcelableExtra(Intent.EXTRA_STREAM)
+            }
+            else -> null
+        }?.takeIf { it.toString().endsWith(".zip", ignoreCase = true) }
+
         setContent {
             KernelSUTheme {
                 val navController = rememberNavController()
@@ -84,6 +97,18 @@ class MainActivity : ComponentActivity() {
 
                 val bottomBarRoutes = remember {
                     BottomBarDestination.entries.map { it.direction.route }.toSet()
+                }
+
+                val navigator = navController.rememberDestinationsNavigator()
+
+                LaunchedEffect(zipUri) {
+                    if (zipUri != null) {
+                        navigator.navigate(
+                            FlashScreenDestination(
+                                FlashIt.FlashModules(listOf(zipUri))
+                            )
+                        )
+                    }
                 }
 
                 Scaffold(

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Flash.kt
@@ -1,5 +1,6 @@
 package me.weishu.kernelsu.ui.screen
 
+import android.content.Context
 import android.net.Uri
 import android.os.Environment
 import android.os.Parcelable
@@ -39,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,6 +56,8 @@ import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.component.KeyEventBlocker
+import me.weishu.kernelsu.ui.component.rememberConfirmDialog
+import me.weishu.kernelsu.ui.component.ConfirmResult
 import me.weishu.kernelsu.ui.util.FlashResult
 import me.weishu.kernelsu.ui.util.LkmSelection
 import me.weishu.kernelsu.ui.util.LocalSnackbarHost
@@ -112,12 +116,49 @@ fun FlashScreen(navigator: DestinationsNavigator, flashIt: FlashIt) {
         mutableStateOf(FlashingStatus.FLASHING)
     }
 
-    LaunchedEffect(Unit) {
-        if (text.isNotEmpty()) {
+    val context = LocalContext.current
+
+    val confirmDialog = rememberConfirmDialog()
+    var confirmed by rememberSaveable { mutableStateOf(flashIt !is FlashIt.FlashModules) }
+    var pendingFlashIt by rememberSaveable { mutableStateOf<FlashIt?>(null) }
+
+    LaunchedEffect(flashIt) {
+        if (flashIt is FlashIt.FlashModules && !confirmed) {
+            val uris = flashIt.uris
+            val moduleNames =
+                uris.mapIndexed { index, uri -> "\n${index + 1}. ${uri.getFileName(context)}" }
+                    .joinToString("")
+            val confirmContent =
+                context.getString(R.string.module_install_prompt_with_name, moduleNames)
+            val confirmTitle = context.getString(R.string.module)
+            val result = confirmDialog.awaitConfirm(
+                title = confirmTitle,
+                content = confirmContent,
+                markdown = true
+            )
+
+            if (result == ConfirmResult.Confirmed) {
+                confirmed = true
+                pendingFlashIt = flashIt
+            } else {
+                // User cancelled, go back
+                navigator.popBackStack()
+            }
+        } else {
+            confirmed = true
+            pendingFlashIt = flashIt
+        }
+    }
+
+    var flashingStarted by rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect(confirmed, pendingFlashIt) {
+        if (!confirmed || pendingFlashIt == null || flashingStarted) {
             return@LaunchedEffect
         }
+        flashingStarted = true
         withContext(Dispatchers.IO) {
-            flashIt(flashIt, onStdout = {
+            flashIt(pendingFlashIt!!, onStdout = {
                 tempText = "$it\n"
                 if (tempText.startsWith("[H[J")) { // clear command
                     text = tempText.substring(6)
@@ -203,6 +244,19 @@ fun FlashScreen(navigator: DestinationsNavigator, flashIt: FlashIt) {
             )
         }
     }
+}
+
+fun Uri.getFileName(context: Context): String {
+    val contentResolver = context.contentResolver
+    val cursor = contentResolver.query(this, null, null, null, null)
+    return cursor?.use {
+        val nameIndex = it.getColumnIndex(android.provider.OpenableColumns.DISPLAY_NAME)
+        if (it.moveToFirst() && nameIndex != -1) {
+            it.getString(nameIndex)
+        } else {
+            this.lastPathSegment ?: "unknown.zip"
+        }
+    } ?: (this.lastPathSegment ?: "unknown.zip")
 }
 
 @Parcelize