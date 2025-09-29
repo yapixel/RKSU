38c8e2e43d45fc743104cd95707bad113229a384
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-29 17:19:40 +0700

    kernel: add older vfs_getattr compat
    * For kernel below 4.11
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- kernel/throne_tracker.c
+++ kernel/throne_tracker.c
@@ -188,7 +188,11 @@ FILLDIR_RETURN_TYPE user_data_actor(struct dir_context *ctx, const char *name,
 	}
 
 	struct kstat stat;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0) || defined(KSU_HAS_NEW_VFS_GETATTR)
 	err = vfs_getattr(&path, &stat, STATX_UID, AT_STATX_SYNC_AS_STAT);
+#else
+	err = vfs_getattr(&path, &stat);
+#endif
 	path_put(&path);
 
 	if (err) {
