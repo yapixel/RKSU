c98bf2fb0482e6377c7545fbe46f699e5ca71e2b
Faris <rissu.ntk@gmail.com>
2025-11-06 10:49:08 +0700

    Revert "kernel: remove unused workqueue"
    This reverts commit d52d01910b276c804b391b3f0995982f772adb67.
    
    Signed-off-by: Faris <rissu.ntk@gmail.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -5,6 +5,7 @@
 #include <generated/utsrelease.h>
 #include <generated/compile.h>
 #include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
+#include <linux/workqueue.h>
 
 #include "allowlist.h"
 #include "arch.h"
@@ -13,6 +14,13 @@
 #include "klog.h" // IWYU pragma: keep
 #include "ksu.h"
 #include "throne_tracker.h"
+
+static struct workqueue_struct *ksu_workqueue;
+
+bool ksu_queue_work(struct work_struct *work)
+{
+	return queue_work(ksu_workqueue, work);
+}
 
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 					void *argv, void *envp, int *flags);
@@ -55,6 +63,8 @@ int __init kernelsu_init(void)
 
 	ksu_core_init();
 
+	ksu_workqueue = alloc_ordered_workqueue("kernelsu_work_queue", 0);
+
 	ksu_allowlist_init();
 
 	ksu_throne_tracker_init();
@@ -80,6 +90,8 @@ void kernelsu_exit(void)
 	ksu_throne_tracker_exit();
 
 	ksu_observer_exit();
+
+	destroy_workqueue(ksu_workqueue);
 
 #ifdef KSU_KPROBE_HOOK
 	ksu_ksud_exit();
